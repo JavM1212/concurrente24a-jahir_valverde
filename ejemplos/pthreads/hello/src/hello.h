5320dbd9b15da66b1ec55c52c2b37772ff304f0c
Jahir Valverde <jahirgabvalmur@gmail.com>
2024-03-16 17:02:37 -0600

    hello.h de ejemplo hello con pthreads modificado

--- ejemplos/pthreads/hello/src/hello.h
+++ ejemplos/pthreads/hello/src/hello.h
@@ -1,7 +1,7 @@
 // Copyright 2024 Jahir Valverde <jahir.valverde@ucr.ac.cr>
 
-#ifndef CONCURRENTE24A_JAHIR_VALVERDE_EJEMPLOS_PTHREADS_HELLO_SRC_HELLO_H_
-#define CONCURRENTE24A_JAHIR_VALVERDE_EJEMPLOS_PTHREADS_HELLO_SRC_HELLO_H_
+#ifndef HELLO_H_
+#define HELLO_H_
 
 #include <pthread.h>
 #include <stdio.h>
@@ -21,4 +21,4 @@ int main(void);
  */
 void* greet(void* data);
 
-#endif  // CONCURRENTE24A_JAHIR_VALVERDE_EJEMPLOS_PTHREADS_HELLO_SRC_HELLO_H_
+#endif  // HELLO_H_
