2a46ef911315acab3f5b868498631790fc722960
Jahir Valverde <jahirgabvalmur@gmail.com>
2024-03-18 17:17:37 -0600

    Comentarios reformulados

--- ejemplos/pthreads/hello/src/hello.c
+++ ejemplos/pthreads/hello/src/hello.c
@@ -2,30 +2,27 @@
 
 #include "hello.h"
 
-// procedure main
 int main(void) {
-    // create_thread(greet)
     pthread_t thread;   // no es necesario inicializarlo porque de eso se 
                         // encarga phtrad_create
     // si phtread_create devuelve un int distinto  0 es que dio error 
     int error = pthread_create(&thread, /*attr*/ NULL, greet, /*arg*/ NULL);
     if (error == EXIT_SUCCESS) {
-        // print "Hello from main thread"
         // sleep(1);  // Pausa el hilo actual 1 segundo
         fprintf(stdout, "Hello from main thread\n");  // stdout
-        pthread_join(thread, /*value_ptr*/ NULL);  // Equivalente al destroy
+        // "Equivalente" al destroy para threads, permite almacenar valores
+        // de retorno y evita condiciones de carrera
+        pthread_join(thread, /*value_ptr*/ NULL);   
     } else {
-        // print "Hello from main thread"
         fprintf(stderr, "Could not create secondary thread\n");  // stderr
     }
 
     return error;  // devolver el error al sistema operativo
-}  // end procedure
+} 
 
-// procedure greet
+// funcion que es llamada por el hilo secundario
 void* greet(void* data) {
     (void) data;
-    // print "Hello from secondary thread"
     fprintf(stdout, "Hello from secondary thread\n");
     return NULL;
-}  // end procedure
+}
