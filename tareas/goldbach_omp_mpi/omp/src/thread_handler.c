d9b67b04174fb018f984e8f05d4ba161082a4fec
Jahir Valverde <jahirgabvalmur@gmail.com>
2024-06-30 08:35:10 -0600

    linted

--- tareas/goldbach_omp_mpi/omp/src/thread_handler.c
+++ tareas/goldbach_omp_mpi/omp/src/thread_handler.c
@@ -12,12 +12,12 @@ void create_threads(input_t* input, int64_t thread_count) {
   // }
 
   #pragma omp parallel num_threads(thread_count) \
-    default(none) shared(input)
-  {
+    default(none) shared(input) {
     // mapeo dinamico
     #pragma omp for schedule(dynamic)
     for (int iteration = 0; iteration < input->size; ++iteration) {
-      goldbach_number_t* my_gold_number = calc_goldbach_number(input->input_arr[iteration]);
+      goldbach_number_t* my_gold_number
+        = calc_goldbach_number(input->input_arr[iteration]);
 
       input->goldbach_numbers[iteration] = my_gold_number;
       input->goldbach_numbers_index++;
